a157f99ea971373777621f96d5ee0ee93c4fba93
kalinbowden <kalinbowden@gmail.com>
2018-04-28 02:47:38 -0500

    last windoq

--- index.js
+++ index.js
@@ -4,20 +4,40 @@ const BrowserWindow = electron.remote.BrowserWindow
 
 const notifyBtn = document.getElementById('CA')
 const MP = document.getElementById('MP')
+const AI = document.getElementById('AI')
 
-notifyBtn.addEventListener('click', function() {
+
+if (notifyBtn)
+{
+    notifyBtn.addEventListener('click', function() {
   const modalPath = path.join('file://', __dirname, 'CreateAccount.html')
   let win = new BrowserWindow({ width: 550, height: 535 })
   win.on('close', function () { win = null })
   win.loadURL(modalPath)
   win.show()
 })
+}
 
 
-MP.addEventListener('click', function() {
+if (MP)
+{
+    MP.addEventListener('click', function() {
   const modalPath = path.join('file://', __dirname, 'MainMenu.html')
   let win = new BrowserWindow({ width: 550, height: 535 })
   win.on('close', function () { win = null })
   win.loadURL(modalPath)
   win.show()
 })
+}
+
+
+if (AI)
+{
+    AI.addEventListener('click', function() {
+  const modalPath = path.join('file://', __dirname, 'AddInventoy.html')
+  let win = new BrowserWindow({ width: 550, height: 535 })
+  win.on('close', function () { win = null })
+  win.loadURL(modalPath)
+  win.show()
+})
+}
