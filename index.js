a2be915b064ae283fe3e93ca9e0ca6b66138f275
kalinbowden <kalinbowden@gmail.com>
2018-04-28 01:49:31 -0500

    hey a third page

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const path = require('path')
 const BrowserWindow = electron.remote.BrowserWindow
 
 const notifyBtn = document.getElementById('CA')
+const MP = document.getElementById('MP')
 
 notifyBtn.addEventListener('click', function() {
   const modalPath = path.join('file://', __dirname, 'CreateAccount.html')
+  let win = new BrowserWindow({ width: 550, height: 535 })
+  win.on('close', function () { win = null })
+  win.loadURL(modalPath)
+  win.show()
+})
+
+
+MP.addEventListener('click', function() {
+  const modalPath = path.join('file://', __dirname, 'MainMenu.html')
   let win = new BrowserWindow({ width: 550, height: 535 })
   win.on('close', function () { win = null })
   win.loadURL(modalPath)
