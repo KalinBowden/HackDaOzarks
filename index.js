f1f20e6c6e2d91501961b1df95ae1b0ff9304b23
HART <kh0790181@otc.edu>
2018-04-28 01:40:26 -0500

    more

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const notifyBtn = document.getElementById('CA')
 
 notifyBtn.addEventListener('click', function() {
   const modalPath = path.join('file://', __dirname, 'CreateAccount.html')
-  let win = new BrowserWindow({ width: 400, height: 200 })
+  let win = new BrowserWindow({ width: 550, height: 535 })
   win.on('close', function () { win = null })
   win.loadURL(modalPath)
   win.show()
