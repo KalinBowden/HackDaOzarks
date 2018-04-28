cc7121fb8f45955568a613e38cf33da2095ee496
kalinbowden <kalinbowden@gmail.com>
2018-04-28 03:36:26 -0500

    save point

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const BrowserWindow = electron.remote.BrowserWindow
 const notifyBtn = document.getElementById('CA')
 const MP = document.getElementById('MP')
 const AI = document.getElementById('AI')
+const create = document.getElementById('create')
+
+// Create Account vars
+var inputEmail4 = document.getElementById('inputEmail4')
+var inputPassword4 = document.getElementById('inputPassword4')
+var inputAddress = document.getElementById('inputAddress')
+var inputAddress2 = document.getElementById('inputAddress2')
+var inputCity = document.getElementById('inputCity')
+var inputState = document.getElementById('inputState')
+var inputZip = document.getElementById('inputZip')
+
+
 
 
 if (notifyBtn)
@@ -41,3 +53,32 @@ if (AI)
   win.show()
 })
 }
+
+
+// account creation
+if (create)
+{
+    create.addEventListener('click', function()
+{
+    var temp = "";
+
+    temp += inputEmail4.value;
+    temp += " ";
+    temp += inputPassword4.value;
+    temp += " ";
+    temp += inputAddress.value;
+    temp += " ";
+    temp += inputAddress2.value;
+    temp += " ";
+    temp += inputCity.value;
+    temp += " "; 
+    temp += inputState.value;
+    temp += " ";
+    temp += inputZip.value;
+    temp += " ";
+
+    alert(temp);    
+} )
+}
+
+
